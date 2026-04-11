f378b91fccaa56d35d6e92ccf9011f38d739e3d9
Sumit Bhasagi <bhasagisumit@gmail.com>
2026-04-11 19:13:20 +0530

    Pattern 6

--- Patterns/Pattern6.cpp
+++ Patterns/Pattern6.cpp
@@ -1,10 +1,9 @@
-// Pattern 6
+// Pattern 7
 
 // 1
-// 2 3
-// 3 4 5
-// 4 5 6 7
-// 5 6 7 8 9
+// 2 1
+// 3 2 1
+// 4 3 2 1
 
 #include <iostream>
 using namespace std;
@@ -12,17 +11,17 @@ using namespace std;
 int main()
 {
   int n;
-  int count = 1;
   cout << "Enter the value of n: ";
   cin >> n;
+  int count = 1;
 
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n; i++)
   {
     count = i;
-    for (int j = 1; j <= i; j++)
+    for (int j = 0; j < i; j++)
     {
       cout << count << " ";
-      count++;
+      count--;
     }
     cout << endl;
   }
