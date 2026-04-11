9d174eaae3d41857e29a7454bc3a9d2f0133a772
Sumit Bhasagi <bhasagisumit@gmail.com>
2026-04-11 20:42:58 +0530

    Pattern 8

---
+++ Patterns/Pattern8.cpp
@@ -0,0 +1,28 @@
+// Pattern 9
+
+// A B C
+// A B C
+// A B C
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter the value of n";
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+    char ch = 'A';
+    for (int j = 0; j < n; j++)
+    {
+      cout << ch << " ";
+      ch++;
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
