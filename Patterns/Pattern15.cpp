9153c7b90bf81d0a974948efc993820840829a3d
Sumit Bhasagi <bhasagisumit@gmail.com>
2026-04-12 18:12:35 +0530

    Pattern 15

---
+++ Patterns/Pattern15.cpp
@@ -0,0 +1,35 @@
+// Pattern 16
+
+// 1 1 1 1
+//   2 2 2
+//     3 3
+//       4
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter the value of n: ";
+  cin >> n;
+
+  for (int i = 1; i <= n; i++)
+  {
+    int space = 1;
+    while (space < i)
+    {
+      cout << "  ";
+      space++;
+    }
+
+    for (int j = 0; j < n - i; j++)
+    {
+      cout << i << " ";
+    }
+
+    cout << endl;
+  }
+
+  return 0;
+}
