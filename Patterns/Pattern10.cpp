c5ae9082df84c611222d085e23e7734ae2f8d3ec
Sumit Bhasagi <bhasagisumit@gmail.com>
2026-04-12 16:32:25 +0530

    Pattern 11

---
+++ Patterns/Pattern10.cpp
@@ -0,0 +1,29 @@
+// Pattern 11
+
+// A
+// B C
+// C D E
+// D E F G
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter the value of n: ";
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+    char ch = 'A' + i - 1;
+    for (int j = 0; j < i; j++)
+    {
+      cout << ch << " ";
+      ch++;
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
---
+++ Patterns/Pattern11.cpp
@@ -0,0 +1,29 @@
+// Pattern 12
+
+// D
+// C D
+// B C D
+// A B C D
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter the value of n: ";
+  cin >> n;
+
+  for (int i = 0; i <= n; i++)
+  {
+    char ch = 'A' + n - i;
+    for (int j = 0; j < i; j++)
+    {
+      cout << ch << " ";
+      ch++;
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
